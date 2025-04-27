4e66e97b39813b0a16bb7302bd361ace5b066986
jchoi4497 <jchoi4497@gmail.com>
2025-04-27 16:54:21 -0700

    refactored shoulders same manner

--- src/ShouldersWorkout.js
+++ src/ShouldersWorkout.js
@@ -2,14 +2,7 @@ import { useState } from "react"
 import TableHead from "./TableHead"
 import TableRow from "./TableRow"
 
-function ShouldersWorkout({ target, reps}){
-// Rear Delt 
-
-    const [rearDeltExercise, setRearDeltExercise] = useState("rdm")
-
-    const handleSelectRearDeltExercise = (option) => {
-        setRearDeltExercise(option)
-    }
+function ShouldersWorkout({ target, reps, label, inputs, onInput }){
 
     const rearDeltExerciseOptions = [
         {label: "Rear Delt Machine", value: "rdm"},
@@ -20,12 +13,6 @@ function ShouldersWorkout({ target, reps}){
         {label: "Cable Face Pulls", value: "cfp"},
     ]
 
-    const [rearDeltTwoExercise, setRearDeltTwoExercise] = useState("1acrdf2")
-
-    const handleSelectRearDeltTwoExercise = (option) => {
-        setRearDeltTwoExercise(option)
-    }
-
     const rearDeltTwoExerciseOptions = [
         {label: "1 Arm Cable Rear Delt Flys", value: "1acrdf2"},
         {label: "Cable Rear Delt Flys", value: "crdf2"},
@@ -35,38 +22,17 @@ function ShouldersWorkout({ target, reps}){
         {label: "Cable Face Pulls", value: "cfp2"},
     ]
 
-// Lat Raises
-
-    const [LatRaisesExercise, setLatRaisesExercise] = useState("dlr")
-
-    const handleSelectLatRaisesExercise = (option) => {
-        setLatRaisesExercise(option)
-    }
-
     const LatRaisesExerciseOptions = [
         {label: "Dumbbell Lat Raises", value: "dlr"},
         {label: "1 Arm Cable Lat Raises", value: "1acdlr" },
         {label: "Machine Lat Raises", value: "mlr"},
     ]
 
-    const [LatRaisesTwoExercise, setLatRaisesTwoExercise] = useState("1acdlr2")
-
-    const handleSelectLatRaisesTwoExercise = (option) => {
-        setLatRaisesTwoExercise(option)
-    }
-
     const LatRaisesTwoExerciseOptions = [
-        {label: "1 Arm Cable Lat Raises", value: "1acdlr2" },
+        {label: "1 Arm Cable Lat Raises", value: "1aclr2" },
         {label: "Dumbbell Lat Raises", value: "dlr2"},
         {label: "Machine Lat Raises", value: "mlr2"},
     ]
-
-// Wrist Curls
-    const [wristCurlsExercise, setWristCurlsExercise] = useState("cwc")
-
-    const handleSelectWristCurlsExercise = (option) => {
-        setWristCurlsExercise(option)
-    }
 
     const wristCurlOptions = [
         {label: "Cable Wrist Curls w/ Bar", value: "cwc"},
@@ -74,72 +40,60 @@ function ShouldersWorkout({ target, reps}){
         {label: "Dumbbell Wrist Curls", value: "dbwc" },
     ]
 
-// Reverse Wrist Curls
-    const [reverseWristCurlsExercise, setReverseWristCurlsExercise] = useState("rdbwc")
-
-    const handleSelectReverseWristCurlsExercise = (option) => {
-        setReverseWristCurlsExercise(option)
-    }
-
     const reverseWristCurlOptions = [
         {label: "Reverse Dumbbell Wrist Curls", value: "rdbwc" },
         {label: "Forearm Curls w/ Easy Bar", value: "fc" },
     ]
 
+    const [exercises, setExercises] = useState([
+        {id: "reardelt", selected: "rdm", options: rearDeltExerciseOptions},
+        {id: "latraise", selected: "dlr", options: LatRaisesExerciseOptions},
+        {id: "reardelt2", selected: "1acrdf2", options: rearDeltTwoExerciseOptions},
+        {id: "latraise2", selected: "1aclr2", options: LatRaisesTwoExerciseOptions},
+        {id: "wristcurl", selected: "cwc", options: wristCurlOptions},
+        {id: "reversewristcurl", selected: "rdbwc", options: reverseWristCurlOptions},
+    ])
+
+    const handleExerciseChange = (rowId, newExerciseValue) => {
+        const exerciseOptions = exercises.map((exercise) => 
+            exercise.id === rowId
+                ? { ...exercise, selected: newExerciseValue }
+                : exercise)
+            
+        setExercises(exerciseOptions)
+        onInput(rowId, newExerciseValue, -1 )
+
+    }
+
+    const handleInputChange = (rowId, selected, index, inputValue ) => {
+        onInput(rowId, selected, index, inputValue)
+    }
+
     return (
-     <div className="flex justify-center font-serif bg-white">
+        <div className="flex justify-center font-serif bg-white">
  
-         <table className="border w-full">
-            <caption className="text-xl font-bold mb-4 border-t"> {target?.label} - {reps?.label} </caption>
-            <TableHead reps={reps} />
-            
-            <tbody className="border px-4 py-2">
-                <TableRow
-                onChange={handleSelectRearDeltExercise}
-                value={rearDeltExercise}
-                options={rearDeltExerciseOptions}
-                reps={reps}
-                rowId='reardelt1'
-                />
-                <TableRow
-                onChange={handleSelectLatRaisesExercise}
-                value={LatRaisesExercise}
-                options={LatRaisesExerciseOptions}
-                reps={reps}
-                rowId='latraise1'
-                />
-                <TableRow
-                onChange={handleSelectRearDeltTwoExercise}
-                value={rearDeltTwoExercise}
-                options={rearDeltTwoExerciseOptions}
-                reps={reps}
-                rowId='reardelt2'
-                />
-                <TableRow
-                onChange={handleSelectLatRaisesTwoExercise}
-                value={LatRaisesTwoExercise}
-                options={LatRaisesTwoExerciseOptions}
-                reps={reps}
-                rowId='latraise2'
-                />
-                <TableRow
-                onChange={handleSelectWristCurlsExercise}
-                value={wristCurlsExercise}
-                options={wristCurlOptions}
-                reps={reps}
-                rowId='wristcurl'
-                />
-                <TableRow 
-                onChange={handleSelectReverseWristCurlsExercise}
-                value={reverseWristCurlsExercise}
-                options={reverseWristCurlOptions}
-                reps={reps}
-                rowId='reversewrist'
-                />
-            </tbody>
-            
-         </table>
-     </div>)
+        <table className="border w-full">
+           <caption className="text-xl font-bold mb-4 border-t"> {label} - {target} </caption>
+           <TableHead reps={reps} />
+           
+           <tbody className="border px-4 py-2">
+               {exercises.map(({ id, selected, options }) => (
+                   <TableRow
+                       key={id}
+                       value={selected}
+                       options={options}
+                       reps={reps}
+                       rowId={id}
+                       inputs={inputs[id]?.input}
+                       onChange={(newOption) => handleExerciseChange(id, newOption)}
+                       cellInput={(index, inputValue) => handleInputChange(id, selected, index, inputValue)}
+                   /> 
+               ))}
+               
+           </tbody>
+
+        </table>
+    </div>)
  }
  
  export default ShouldersWorkout
