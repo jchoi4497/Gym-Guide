f2fd50cae96c5827adc5805d3f7b6cb1da53ce0f
jchoi4497 <jchoi4497@gmail.com>
2025-04-27 16:48:48 -0700

    refactored legs, also made panel length longer for option so it wont change size

--- src/LegsWorkout.js
+++ src/LegsWorkout.js
@@ -2,13 +2,7 @@ import { useState } from "react"
 import TableHead from "./TableHead"
 import TableRow from "./TableRow"
 
-function LegsWorkout({ target, reps }){
-// Squats
-    const [squatExercise, setSquatExercise] = useState("bs")
-
-    const handleSelectSquatExercise = (option) => {
-        setSquatExercise(option)
-    }
+function LegsWorkout({ target, reps, label, inputs, onInput }){
 
     const squatOptions = [
         {label: "Barbell Squats", value: "bs"},
@@ -18,87 +12,77 @@ function LegsWorkout({ target, reps }){
         {label: "Leg Extensions", value: "le"},
     ]
 
-// Split Squats
-const [splitSquatsExercise, setSplitSquatsExercise] = useState("dbss")
 
-const handleSelectSplitSquatsExercise = (option) => {
-    setSplitSquatsExercise(option)
-}
+    const splitSquatsOptions = [
+        {label: "Dumbbell Bulgarian Split Squats", value: "dbss"},
+        {label: "Smith Machine Bulgarian Split Squats", value: "smbss"},
+        {label: "Hack Squats", value: "hs" },
+        {label: "Leg Curls", value: "lc"},
+    ]
 
-const splitSquatsOptions = [
-    {label: "Dumbbell Bulgarian Split Squats", value: "dbss"},
-    {label: "Smith Machine Bulgarian Split Squats", value: "smbss"},
-    {label: "Hack Squats", value: "hs" },
-    {label: "Leg Curls", value: "lc"},
-]
+    const backExtensionOptions = [
+        {label: "Back Extensions", value: "be"},
+        {label: "Romanian Deadlift (RDL)", value: "rdl"},
+        {label: "Dumbbell RDL", value: "dbrdl" },
+        {label: "Deadlift", value: "dl" },
+        {label: "Good Mornings", value: "gm" },
+    ]
 
-// back Extensions
-const [backExtensionExercise, setBackExtensionExercise] = useState("be")
+    const calfRaisesOptions = [
+        {label: "Calf Raise Machine", value: "crm"},
+        {label: "Seated Calf Raises", value: "scr"},
+        {label: "Smith Machine Calf Raises", value: "smcr" },
+        {label: "Barbell Calf Raises", value: "bcr" },
+    ]
 
-const handleSelectBackExtensionExercise = (option) => {
-    setBackExtensionExercise(option)
-}
+    const [exercises, setExercises] = useState([
+        {id: "squat", selected: "bs", options: squatOptions},
+        {id: "splitsquat", selected: "dbss", options: splitSquatsOptions},
+        {id: "backextension", selected: "be", options: backExtensionOptions},
+        {id: "calfraise", selected: "crm", options: calfRaisesOptions},
+    ])
 
-const backExtensionOptions = [
-    {label: "Back Extensions", value: "be"},
-    {label: "Romanian Deadlift (RDL)", value: "rdl"},
-    {label: "Dumbbell RDL", value: "dbrdl" },
-    {label: "Deadlift", value: "dl" },
-    {label: "Good Mornings", value: "gm" },
-]
+    const handleExerciseChange = (rowId, newExerciseValue) => {
+        const exerciseOptions = exercises.map((exercise) => 
+            exercise.id === rowId
+                ? { ...exercise, selected: newExerciseValue }
+                : exercise)
+            
+        setExercises(exerciseOptions)
+        onInput(rowId, newExerciseValue, -1 )
 
-// calf raises
-const [calfRaisesExercise, setCalfRaisesExercise] = useState("crm")
+    }
 
-const handleSelectCalfRaisesExercise = (option) => {
-    setCalfRaisesExercise(option)
-}
+    const handleInputChange = (rowId, selected, index, inputValue ) => {
+        onInput(rowId, selected, index, inputValue)
+    }
 
-const calfRaisesOptions = [
-    {label: "Calf Raise Machine", value: "crm"},
-    {label: "Seated Calf Raises", value: "scr"},
-    {label: "Smith Machine Calf Raises", value: "smcr" },
-    {label: "Barbell Calf Raises", value: "bcr" },
-]
 
     return (
-     <div className="flex justify-center font-serif bg-white">
-         <table className="border w-full">
-            <caption className="text-xl font-bold mb-4 border-t"> {target?.label} - {reps?.label} </caption>
-            <TableHead reps={reps} />
-            
-            <tbody className="border px-4 py-2">
-                <TableRow
-                onChange={handleSelectSquatExercise}
-                value={squatExercise}
-                options={squatOptions}
-                reps={reps}
-                rowId='squats'
-                />
-                <TableRow 
-                onChange={handleSelectSplitSquatsExercise}
-                value={splitSquatsExercise}
-                options={splitSquatsOptions}
-                reps={reps}
-                rowId='splits'
-                />
-                <TableRow
-                onChange={handleSelectBackExtensionExercise}
-                value={backExtensionExercise}
-                options={backExtensionOptions}
-                reps={reps}
-                rowId='backextension'
-                />
-                <TableRow
-                onChange={handleSelectCalfRaisesExercise}
-                value={calfRaisesExercise}
-                options={calfRaisesOptions}
-                reps={reps}
-                rowId='calf'
-                />
-            </tbody>
-         </table>
-     </div>)
+        <div className="flex justify-center font-serif bg-white">
+ 
+        <table className="border w-full">
+           <caption className="text-xl font-bold mb-4 border-t"> {label} - {target} </caption>
+           <TableHead reps={reps} />
+           
+           <tbody className="border px-4 py-2">
+               {exercises.map(({ id, selected, options }) => (
+                   <TableRow
+                       key={id}
+                       value={selected}
+                       options={options}
+                       reps={reps}
+                       rowId={id}
+                       inputs={inputs[id]?.input}
+                       onChange={(newOption) => handleExerciseChange(id, newOption)}
+                       cellInput={(index, inputValue) => handleInputChange(id, selected, index, inputValue)}
+                   /> 
+               ))}
+               
+           </tbody>
+
+        </table>
+    </div>)
  }
  
  export default LegsWorkout
