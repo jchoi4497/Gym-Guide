bbb3bd2d57cf901c4a3a02f57afa527e59e478f3
jchoi4497 <jchoi4497@gmail.com>
2025-04-27 16:42:43 -0700

    refactored backworkout to be like chest, added onInput as prop to all workouts

--- src/BackWorkout.js
+++ src/BackWorkout.js
@@ -2,13 +2,7 @@ import { useState } from "react"
 import TableHead from "./TableHead"
 import TableRow from "./TableRow"
 
-function BackWorkout({ target, reps}){
-// Pull Ups
-    const [pullUpExercise, setPullUpExercise] = useState("pu")
-
-    const handleSelectPullUpExercise = (option) => {
-        setPullUpExercise(option)
-    }
+function BackWorkout({ target, reps, label, inputs, onInput}){
 
     const pullUpOptions = [
         {label: "Pull Ups", value: "pu"},
@@ -17,13 +11,6 @@ function BackWorkout({ target, reps}){
         {label: "Band Assist Pull Ups", value: "bapu"},
         {label: "Negative Pull Ups", value: "npu"},
     ]
-
-// Rows
-    const [rowsExercise, setRowsExercise] = useState("mr")
-
-    const handleSelectRowsExercise = (option) => {
-        setRowsExercise(option)
-    }
 
     const rowsOptions = [
         {label: "Machine Rows", value: "mr"},
@@ -35,13 +22,6 @@ function BackWorkout({ target, reps}){
         {label: "Cable Rows w/ Bar", value: "crb"},
     ]
 
-// Lat exercises
-    const [latsExercise, setLatsExercise] = useState("lpd2")
-
-    const handleSelectLatsExercise = (option) => {
-        setLatsExercise(option)
-    }
-
     const latsOptions = [
         {label: "Lat Pull Downs", value: "lpd2"},
         {label: "Assisted Pull Ups", value: "apu2" },
@@ -50,81 +30,68 @@ function BackWorkout({ target, reps}){
         {label: "Straight Arm Pull Downs", value: "sapd"},
     ]
 
-// Bicep exercises
-const [bicepExercise, setBicepExercise] = useState("dbc")
+    const bicepOptions = [
+        {label: "Dumbbell Bicep Curls", value: "dbc"},
+        {label: "Straight Bar Bicep Curls", value: "sbbc" },
+        {label: "Straight Bar Preacher Curls", value: "sbpc"},
+        {label: "Dumbbell Preacher Curls", value: "dpc"},
+        {label: "Cable Curls w/ Bar Grip", value: "ccbg"},
+        {label: "Machine Bicep Curls", value: "mbc"},
+    ]
 
-const handleSelectBicepExercise = (option) => {
-    setBicepExercise(option)
-}
+    const bicepTwoOptions = [
+        {label: "Straight Bar Preacher Curls", value: "sbpc2"},
+        {label: "Straight Bar Bicep Curls", value: "sbbc2" },
+        {label: "Dumbbell Bicep Curls", value: "dbc2"},
+        {label: "Dumbbell Preacher Curls", value: "dpc2"},
+        {label: "Cable Curls w/ Bar Grip", value: "ccbg2"},
+        {label: "Machine Bicep Curls", value: "mbc2"},
+    ]
 
-const bicepOptions = [
-    {label: "Dumbbell Bicep Curls", value: "dbc"},
-    {label: "Straight Bar Bicep Curls", value: "sbbc" },
-    {label: "Straight Bar Preacher Curls", value: "sbpc"},
-    {label: "Dumbbell Preacher Curls", value: "dpc"},
-    {label: "Cable Curls w/ Bar Grip", value: "ccbg"},
-    {label: "Machine Bicep Curls", value: "mbc"},
-]
+    const [exercises, setExercises] = useState([
+        {id: "pullup", selected: "pu", options: pullUpOptions},
+        {id: "row", selected: "mr", options: rowsOptions},
+        {id: "lat", selected: "lpd2", options: latsOptions},
+        {id: "bicep", selected: "dbc", options: bicepOptions},
+        {id: "bicep2", selected: "sbpc2", options: bicepTwoOptions},
+    ])
 
-const [bicepTwoExercise, setBicepTwoExercise] = useState("dbpc2")
+    const handleExerciseChange = (rowId, newExerciseValue) => {
+        const exerciseOptions = exercises.map((exercise) => 
+            exercise.id === rowId
+                ? { ...exercise, selected: newExerciseValue }
+                : exercise)
+            
+        setExercises(exerciseOptions)
+        onInput(rowId, newExerciseValue, -1 )
 
-const handleSelectBicepTwoExercise = (option) => {
-    setBicepTwoExercise(option)
-}
+    }
 
-const bicepTwoOptions = [
-    {label: "Straight Bar Preacher Curls", value: "dbpc2"},
-    {label: "Straight Bar Bicep Curls", value: "sbbc2" },
-    {label: "Dumbbell Bicep Curls", value: "dbc2"},
-    {label: "Dumbbell Preacher Curls", value: "dpc2"},
-    {label: "Cable Curls w/ Bar Grip", value: "ccbg2"},
-    {label: "Machine Bicep Curls", value: "mbc2"},
-]
-
+    const handleInputChange = (rowId, selected, index, inputValue ) => {
+        onInput(rowId, selected, index, inputValue)
+    }
 
     return (
      <div className="flex justify-center font-serif bg-white">
  
          <table className="border w-full">
-            <caption className="text-xl font-bold mb-4 border-t"> {target?.label} - {reps?.label} </caption>
+            <caption className="text-xl font-bold mb-4 border-t"> {label} - {target} </caption>
             <TableHead reps={reps} />
             
             <tbody className="border px-4 py-2">
-                <TableRow
-                    onChange={handleSelectPullUpExercise}
-                    value={pullUpExercise}
-                    options={pullUpOptions}
-                    reps={reps}
-                    rowId='pullup'
-                />
-                <TableRow
-                    onChange={handleSelectRowsExercise}
-                    value={rowsExercise}
-                    options={rowsOptions}
-                    reps={reps}
-                    rowId='rows'
-                />
-                <TableRow
-                    onChange={handleSelectLatsExercise}
-                    value={latsExercise}
-                    options={latsOptions}
-                    reps={reps}
-                    rowId='lats'
-                />
-                <TableRow
-                    onChange={handleSelectBicepExercise}
-                    value={bicepExercise}
-                    options={bicepOptions}
-                    reps={reps}
-                    rowId='bicep1'
-                />
-                <TableRow
-                    onChange={handleSelectBicepTwoExercise}
-                    value={bicepTwoExercise}
-                    options={bicepTwoOptions}
-                    reps={reps}
-                    rowId='bicep2'
-                /> 
+                {exercises.map(({ id, selected, options }) => (
+                    <TableRow
+                        key={id}
+                        value={selected}
+                        options={options}
+                        reps={reps}
+                        rowId={id}
+                        inputs={inputs[id]?.input}
+                        onChange={(newOption) => handleExerciseChange(id, newOption)}
+                        cellInput={(index, inputValue) => handleInputChange(id, selected, index, inputValue)}
+                    /> 
+                ))}
+                
             </tbody>
 
          </table>
