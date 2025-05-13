a3d6c995bc80c6922e87b6ce94443cd21e33b00d
jonathan choi <jchoi4497@gmail.com>
2025-05-12 19:08:02 -0700

    Created seperate exerciseList file to display full name of workout

---
+++ src/exerciseNames.js
@@ -0,0 +1,139 @@
+
+
+const exerciseNames = {
+  // incline
+  dip: "Dumbbell Incline Press",
+  mip: "Machine Incline Press",
+  smip: "Smith Machine Incline Press",
+  bip: "Barbell Incline Press",
+
+  // chest press
+  dp: "Dumbbell Press",
+  mp: "Machine Press",
+  smp: "Smith Machine Press",
+  bp: "Barbell Press",
+
+  // flies
+  cfm: "Chest Fly Machine",
+  cf: "Cable Flys",
+  df: "Dumbbell Flys",
+
+  // triceps1
+  sbcpd: "Straight Bar Cable Push Downs",
+  rpd: "Rope Pull Downs",
+  oacpd: "1 Arm Cable Pull Downs",
+  obce: "Overhead Bar Cable Extensions",
+  ode: "Overhead Dumbbell Extensions",
+  d: "Dips",
+
+  // triceps2
+  oacpdt: "1 Arm Cable Pull Downs",
+  rpdt: "Rope Pull Downs",
+  sbcpd: "Straight Bar Cable Push Downs",
+  obce: "Overhead Bar Cable Extensions",
+  ode: "Overhead Dumbbell Extensions",
+  dt: "Dips",
+
+  // pullups
+  pu: "Pull Ups",
+  apu: "Assisted Pull Ups",
+  lpd: "Lat Pull Downs",
+  bapu: "Band Assist Pull Ups",
+  npu: "Negative Pull Ups",
+
+  // rows
+  mr: "Machine Rows",
+  borb: "Bent Over Rows Barbell",
+  bord: "Bent Over Rows Dumbbell",
+  oabor: "1 Arm Bent Over Rows",
+  tbr: "T-bar row",
+  croa: "Cable Rows 1 Arm",
+  crb: "Cable Rows w/ Bar",
+
+  // lats
+  lpdt: "Lat Pull Downs",
+  aput: "Assisted Pull Ups",
+  vbpd: "V-bar Pull Downs",
+  nglpd: "Neutral Grip Lat Pull Downs",
+  sapd: "Straight Arm Pull Downs",
+
+  // biceps
+  dbc: "Dumbbell Bicep Curls",
+  sbbc: "Straight Bar Bicep Curls",
+  sbpc: "Straight Bar Preacher Curls",
+  dpc: "Dumbbell Preacher Curls",
+  ccbg: "Cable Curls w/ Bar Grip",
+  mbc: "Machine Bicep Curls",
+
+  // biceps two
+  sbpct: "Straight Bar Preacher Curls",
+  sbbct: "Straight Bar Bicep Curls",
+  dbct: "Dumbbell Bicep Curls",
+  dpct: "Dumbbell Preacher Curls",
+  ccbgt: "Cable Curls w/ Bar Grip",
+  mbct: "Machine Bicep Curls",
+
+  // squats
+  bs: "Barbell Squats",
+  sms: "Smith Machine Squats",
+  mlp: "Machine Leg Press",
+  gs: "Goblet Squats",
+  le: "Leg Extensions",
+
+  // split squats
+  dbss: "Dumbbell Bulgarian Split Squats",
+  smbss: "Smith Machine Bulgarian Split Squats",
+  hs: "Hack Squats",
+  lc: "Leg Curls",
+
+  // back extensions
+  be: "Back Extensions",
+  rdl: "Romanian Deadlift (RDL)",
+  dbrdl: "Dumbbell RDL",
+  dl: "Deadlift",
+  gm: "Good Mornings",
+
+  // calfs
+  crm: "Calf Raise Machine",
+  scr: "Seated Calf Raises",
+  smcr: "Smith Machine Calf Raises",
+  bcr: "Barbell Calf Raises",
+
+  // rear delts
+  rdm: "Rear Delt Machine",
+  oardm: "1 Arm Rear Delt Machine",
+  crdf: "Cable Rear Delt Flys",
+  oacrdf: "1 Arm Cable Rear Delt Flys",
+  drdf: "Dumbbell Rear Delt Flys",
+  cfp: "Cable Face Pulls",
+
+  // rear delts two
+  oacrdft: "1 Arm Cable Rear Delt Flys",
+  crdft: "Cable Rear Delt Flys",
+  oardmt: "1 Arm Rear Delt Machine",
+  rdmt: "Rear Delt Machine",
+  drdft: "Dumbbell Rear Delt Flys",
+  cfpt: "Cable Face Pulls",
+
+  // lat raises
+  dlr: "Dumbbell Lat Raises",
+  oacdlr: "1 Arm Cable Lat Raises",
+  mlr: "Machine Lat Raises",
+
+  // lat raises two
+  oaclrt: "1 Arm Cable Lat Raises",
+  dlrt: "Dumbbell Lat Raises",
+  mlrt: "Machine Lat Raises",
+
+  // wrist curls
+  cwc: "Cable Wrist Curls w/ Bar",
+  bbbwc: "Barbell Behind Back Wrist Curls",
+  dbwc: "Dumbbell Wrist Curls",
+
+  // reverse wrist curls
+  rdbwc: "Reverse Dumbbell Wrist Curls",
+  fc: "Forearm Curls w/ Easy Bar",
+
+};
+
+export default exerciseNames;
