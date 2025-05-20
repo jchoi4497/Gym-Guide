3aaff542ed5f396fba43ea07dfb29f64c95c4b18
jonathan choi <jchoi4497@gmail.com>
2025-05-19 17:25:18 -0700

    made navbar componenet reuseable and made spacing same on all pages to make more consistent

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <div className="flex space-x-6 mb-6">
+      <Link to="/" className="text-blue-700 hover:underline font-medium">🏠 Home</Link>
+      <Link to="/Hypertrophy" className="text-blue-700 hover:underline font-medium">Hypertrophy</Link>
+      <Link to="/SavedWorkouts" className="text-blue-700 hover:underline font-medium">Saved Workouts</Link>
+    </div>
+  );
+}
+
+export default Navbar;
