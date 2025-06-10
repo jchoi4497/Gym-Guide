9a7b59f107f1ffef1ee8c64b06b3d9880ebeec6c
jonathan choi <jchoi4497@gmail.com>
2025-06-09 21:26:56 -0700

    made it so desc says click pic and made buttons not w-full

--- src/WorkoutType.js
+++ src/WorkoutType.js
@@ -10,13 +10,13 @@ function WorkoutType({ type, image, desc, to }) {
                     className="
                     w-72 h-72 object-cover border-white border-2 border-solid
                     transition-all duration-300
-                    hover:scale-105 hover:saturate-125
+                    hover:scale-110 active:scale-110 hover:saturate-125
                     rounded-2xl shadow-lg mb-6 mt-4
                     hover:border-blue-500 active:border-stone-400
                     "
                 />
             </Link>
-            <p className="text-center w-72 h-12 italic font-serif">{desc}</p>
+            <p className="text-center w-72 h-12 font-serif">{desc}</p>
         </div>
 
     );
