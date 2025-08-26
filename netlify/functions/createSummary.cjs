776338e02cbda0326ac168f0d07cd73f87c7039b
jonathan choi <jchoi4497@gmail.com>
2025-08-25 17:22:13 -0700

    adding max tokens restrictions to response

--- netlify/functions/createSummary.cjs
+++ netlify/functions/createSummary.cjs
@@ -12,6 +12,7 @@ exports.handler = async (event) => {
     const completion = await OpenAi.chat.completions.create({
       model: "gpt-4o",
       messages: [{ role: "user", content: prompt }],
+      max_tokens: 200,
     });
 
     return {
